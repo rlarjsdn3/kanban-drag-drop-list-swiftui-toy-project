527fa475b130bce2eae924376d1bcfd05f3b2552
Zizi_Kim <rlarjsdn3@naver.com>
2023-08-15 22:57:30 +0900

    kanban 프로젝트 생성

---
+++ kanban-list/kanban-list/kanban_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanban_listApp.swift
+//  kanban-list
+//
+//  Created by 김건우 on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct kanban_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
